80b9e21103b67abcf3e1cd6919a44c59010f6d5b
johnlee175 <johnlee175@163.com>
2017-03-27 16:43:03 +0800

    check process running or foreground

--- src/main/java/com/johnsoft/library/util/system/AppPackageUtils.java
+++ src/main/java/com/johnsoft/library/util/system/AppPackageUtils.java
@@ -1,12 +1,21 @@
 package com.johnsoft.library.util.system;
 
+import static android.content.Context.ACTIVITY_SERVICE;
+
 import java.io.File;
 import java.security.MessageDigest;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.ActivityManager;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
@@ -14,6 +23,7 @@ import android.content.pm.PackageManager;
 import android.content.pm.Signature;
 import android.net.Uri;
 import android.os.Binder;
+import android.os.Build;
 
 public final class AppPackageUtils {
     private AppPackageUtils() {
@@ -102,6 +112,121 @@ public final class AppPackageUtils {
                 }
             }
             return allPackagesOk;
+        }
+        return false;
+    }
+
+    /**
+     * Just can check self process is foreground or not.
+     */
+    public static boolean isForegroundProcess(Context context, String packageName) {
+        ActivityManager am = (ActivityManager) context.getSystemService(ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> appProcesses = am.getRunningAppProcesses();
+        List<String> packageNames = new ArrayList<>();
+        if (appProcesses != null) {
+            for (ActivityManager.RunningAppProcessInfo appProcess : appProcesses) {
+                if (appProcess.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
+                    packageNames.add(appProcess.processName);
+                }
+            }
+        }
+        return packageNames.contains(packageName);
+    }
+
+    private static String topActivityPackageName;
+
+    /**
+     * Can check top Activity in any process, but this way exists strict limits of permission.
+     * 1. First Step:
+     * <pre><code> {@code
+     * <manifest xmlns:android="http://schemas.android.com/apk/res/android" xmlns:tools="http://schemas.android.com/tools">
+     *   <uses-permission android:name="android.permission.PACKAGE_USAGE_STATS" tools:ignore="ProtectedPermissions"/>
+     *   <uses-permission android:name="android.permission.GET_TASKS" />
+     * </manifest>
+     * } <code></pre>
+     * 2. Second Step:
+     * <pre><code> {@code
+     * ...
+     * if (Build.VERSION.SDK_INT >= 21) {
+     *      forwardUsageAccessStat();
+     * }
+     * ...
+     * @TargetApi(21)
+     * private void forwardUsageAccessStat() {
+     *      Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
+     *      startActivity(intent);
+     * }
+     * } <code></pre>
+     *
+     */
+    public static synchronized boolean isTopActivty(Context context, String packageName) {
+        final String name;
+        if (Build.VERSION.SDK_INT >= 21) {
+            name = getTopActivtyPackageNameFromUsageStatsService(context);
+        } else {
+            name = getTopActivtyPackageNameFromActivityService(context);
+        }
+        if (name != null) {
+            topActivityPackageName = name;
+        }
+        return packageName.equals(topActivityPackageName);
+    }
+
+    @TargetApi(21)
+    private static String getTopActivtyPackageNameFromUsageStatsService(Context context) {
+        UsageStatsManager usageStatsManager = (UsageStatsManager) context.getSystemService(Context.USAGE_STATS_SERVICE);
+        long time = System.currentTimeMillis();
+        List<UsageStats> stats = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, time - 1000 * 10, time);
+        if (stats != null) {
+            SortedMap<Long, UsageStats> mySortedMap = new TreeMap<>();
+            for (UsageStats usageStats : stats) {
+                mySortedMap.put(usageStats.getLastTimeUsed(), usageStats);
+            }
+            if (!mySortedMap.isEmpty()) {
+                return mySortedMap.get(mySortedMap.lastKey()).getPackageName();
+            }
+        }
+        return null;
+    }
+
+    @TargetApi(14)
+    private static String getTopActivtyPackageNameFromActivityService(Context context) {
+        ActivityManager am = (ActivityManager) context.getSystemService(ACTIVITY_SERVICE);
+        List<ActivityManager.RunningTaskInfo> runningTasks = am.getRunningTasks(1);
+        if (runningTasks != null && !runningTasks.isEmpty()) {
+            ComponentName componentName = runningTasks.get(0).topActivity;
+            if (componentName != null) {
+                return componentName.getPackageName();
+            }
+        }
+        return null;
+    }
+
+    public static void launch(Context context, String packageName) {
+        try {
+            PackageManager pm = context.getPackageManager();
+            PackageInfo packageInfo = pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            if (packageInfo != null) {
+                Intent intent = pm.getLaunchIntentForPackage(packageName);
+                context.startActivity(intent);
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            /* ignore */
+        }
+    }
+
+    public static boolean isForegroundServiceRunning(Context context, String packageName) {
+        ActivityManager am = (ActivityManager) context.getSystemService(ACTIVITY_SERVICE);
+        List<ActivityManager.RunningServiceInfo> runningServices = am.getRunningServices(Integer.MAX_VALUE);
+        if (runningServices != null && !runningServices.isEmpty()) {
+            for (ActivityManager.RunningServiceInfo runningService : runningServices) {
+                if (runningService.foreground) {
+                    String processName = runningService.process;
+                    ComponentName componentName = runningService.service;
+                    return packageName.equals(processName) ||
+                            (componentName != null && packageName.equals(componentName.getPackageName()));
+                }
+            }
         }
         return false;
     }
